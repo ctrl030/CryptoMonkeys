573ece3ad542d8e5766f197efe64ce2c290c7461
ctrl030 <70302284+ctrl030@users.noreply.github.com>
2020-11-25 13:41:10 -0300

    2

---
+++ client/abi.js
@@ -0,0 +1,374 @@
+var abi = [
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "approved",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "Approval",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "parent1Id",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "parent2Id",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "genes",
+        type: "uint256",
+      },
+    ],
+    name: "MonkeyCreated",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "Transfer",
+    type: "event",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "GEN0_Limit",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "contractOwner",
+    outputs: [
+      {
+        internalType: "address payable",
+        name: "",
+        type: "address",
+      },
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "gen0amountTotal",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "monkeys",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "genes",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "birthtime",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "parent1Id",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "parent2Id",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "generation",
+        type: "uint256",
+      },
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_genes",
+        type: "uint256",
+      },
+    ],
+    name: "createGen0Monkey",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "findMonkey",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "genes",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "birthtime",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "parent1Id",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "parent2Id",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "generation",
+        type: "uint256",
+      },
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "approvedAddress",
+        type: "address",
+      },
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+      {
+        internalType: "address",
+        name: "allowedAddress",
+        type: "address",
+      },
+    ],
+    name: "approve",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "name",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "symbol",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "totalSupply",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+    ],
+    name: "balanceOf",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "ownerOf",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        internalType: "address",
+        name: "_to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "_tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "transfer",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
---
+++ client/index.js
@@ -0,0 +1,52 @@
+var web3 = new Web3(Web3.givenProvider);
+
+var instance;
+var user;
+var contractAddress = "0x0D344fADC22DcAcf4Dadc36AEFC9C5e38a14EE29";
+
+$(document).ready(function () {
+  window.ethereum.enable().then(function (accounts) {
+    instance = new web3.eth.Contract(abi, contractAddress, {
+      from: accounts[0],
+    });
+    user = accounts[0];
+
+    console.log(instance);
+
+    $("#mintMonkey").click(() => {
+      let dnaStr = getDna();
+
+      instance.methods
+        .createGen0Monkey(dnaStr)
+        .send({}, function (error, txHash) {
+          if (error) {
+            console.log(err);
+          } else {
+            console.log(txHash);
+          }
+        });
+
+      instance.events
+        .MonkeyCreated()
+        .on("data", function (event) {
+          console.log(event);
+          let owner = event.returnValues.owner;
+          let tokenId = event.returnValues.tokenId;
+          let parent1Id = event.returnValues.parent1Id;
+          let parent2Id = event.returnValues.parent2Id;
+          let genes = event.returnValues.genes;
+          $("#monkeyCreatedDiv").css("display", "flex");
+          $("#monkeyCreatedDiv").text(
+            "Crypto Monkey created successfully! Here are the details: " 
+            + " owner: " + owner 
+            + " tokenId: " + tokenId 
+            + " parent1Id: " + parent1Id 
+            + " parent2Id: " + parent2Id 
+            + " genes: " + genes
+              
+          );
+        })
+        .on("error", console.log(error));
+    });
+  });
+});
