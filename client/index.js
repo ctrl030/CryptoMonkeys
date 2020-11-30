a630fd89f402127d4e23766e20280e7e8e7ce0b8
ctrl030 <70302284+ctrl030@users.noreply.github.com>
2020-11-30 19:21:25 -0300

    6

--- client/index.js
+++ client/index.js
@@ -2,7 +2,7 @@ var web3 = new Web3(Web3.givenProvider);
 
 var instance;
 var user;
-var contractAddress = "0x74d2cC5B4ED5567989093972248fbf9eEB979150";
+var contractAddress = "0xA0e39141dfb59fF8261c1398Cd7d6e1424322B38";
 
 $(document).ready(function () {
   window.ethereum.enable().then(function (accounts) {
@@ -37,16 +37,22 @@ $(document).ready(function () {
           let genes = event.returnValues.genes;
           $("#monkeyCreatedDiv").css("display", "flex");
           $("#monkeyCreatedDiv").text(
-            "Crypto Monkey created successfully! Here are the details: " 
-            + " owner: " + owner 
-            + " tokenId: " + tokenId 
-            + " parent1Id: " + parent1Id
-            + " parent2Id: " + parent2Id 
-            + " genes: " + genes
-              
+            "Crypto Monkey created successfully! Here are the details: " +
+              " owner: " +
+              owner +
+              " tokenId: " +
+              tokenId +
+              " parent1Id: " +
+              parent1Id +
+              " parent2Id: " +
+              parent2Id +
+              " genes: " +
+              genes
           );
         })
-        .on("error", function(error){console.log(error)});
+        .on("error", function (error) {
+          console.log(error);
+        });
     });
   });
 });
