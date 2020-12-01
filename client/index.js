f1424643816321ca1043c8182d1bd913cc21bc40
ctrl030 <70302284+ctrl030@users.noreply.github.com>
2020-12-01 17:30:32 -0300

    8

--- client/index.js
+++ client/index.js
@@ -2,10 +2,9 @@ var web3 = new Web3(Web3.givenProvider);
 
 var instance;
 var user;
-var contractAddress = "0xA0e39141dfb59fF8261c1398Cd7d6e1424322B38";
+var contractAddress = "0x438B9f1Ee8C46e50fCD0d3Cf8bF21E55f2303147";
 
-$(document).ready( async function () {
-
+$(document).ready(async function () {
   var accounts = await window.ethereum.enable();
 
   instance = new web3.eth.Contract(abi, contractAddress, {
@@ -14,18 +13,18 @@ $(document).ready( async function () {
 
   user = accounts[0];
 
-  console.log("instance: " + instance);
+  console.log("instance: ");
+  console.log(instance);
 
-  
+  console.log("user: " + user);
+
+  console.log("accounts[0]: " + accounts[0]);
 });
 
 $("#mintMonkey").click(() => {
-  
   let dnaStr = getDna();
 
-  instance.methods
-  .createGen0Monkey(dnaStr)
-  .send({}, function (error, txHash) {
+  instance.methods.createGen0Monkey(dnaStr).send({}, function (error, txHash) {
     if (error) {
       console.log(error);
     } else {
@@ -33,30 +32,31 @@ $("#mintMonkey").click(() => {
     }
   });
 
-  instance.events.MonkeyCreated()
-  .on("data", function (event) {
-    console.log(event);
-    let owner = event.returnValues.owner;
-    let tokenId = event.returnValues.tokenId;
-    let parent1Id = event.returnValues.parent1Id;
-    let parent2Id = event.returnValues.parent2Id;
-    let genes = event.returnValues.genes;
-    $("#monkeyCreatedDiv").css("display", "flex");
-    $("#monkeyCreatedDiv").text(
-      "Crypto Monkey created successfully! Here are the details: " +
-      " owner: " + owner +
-      " tokenId: " + tokenId +
-      " parent1Id: " + parent1Id +
-      " parent2Id: " + parent2Id +
-      " genes: " + genes
-    );
-  })      
-  .on("error", function (error) {
-    console.log(error);
-  });
-    
-});
-
-
-
+  instance.events
+    .MonkeyCreated()
+    .on("data", function (event) {
+      console.log(event);
+      let owner = event.returnValues.owner;
+      let tokenId = event.returnValues.tokenId;
+      let parent1Id = event.returnValues.parent1Id;
+      let parent2Id = event.returnValues.parent2Id;
+      let genes = event.returnValues.genes;
+      $("#monkeyCreatedDiv").css("display", "flex");
+      $("#monkeyCreatedDiv").text(
+        "Crypto Monkey created successfully! Here are the details: " +
+          " owner: " +
+          owner +
+          " tokenId: " +
+          tokenId +
+          " parent1Id: " +
+          parent1Id +
+          " parent2Id: " +
+          parent2Id +
+          " genes: " +
+          genes
+      );
+    })
+    .on("error", function (error) {
+      console.log(error);
+    });
 });
